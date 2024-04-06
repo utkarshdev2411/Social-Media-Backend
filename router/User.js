8d8ed1a5ed5c3cfe7788e88c42ef35786a3ba15c
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-04-06 13:08:30 +0530

    Refactor user registration and login routes

--- router/User.js
+++ router/User.js
@@ -9,25 +9,45 @@ router.post('/new/user', async (req, res) => {
     const { email, username, password, profile } = req.body;
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(password, salt);
-    console.log(hash);
-try {
-        
-        let user = await User.findOne(email : req.body.email);
+    try {
 
-        
+        let user = await User.findOne({ email: req.body.email });
+
         if (user) {
             return res.status(200).json("User already exists");
-        }else{
+        } else {
+            user = await User.create({ email: email, username: username, password: hash, profile: profile });
 
-
-        user = await User.create({ email: email, username: username, password: hash, profile: profile });
-
-        const accessToken = jwt.sign({ id: user._id, username: user.username },
-            process.env.JWT_SECRET);
-        res.status(200).json({ user, accessToken });
-
+            const accessToken = jwt.sign({ id: user._id, username: user.username },
+                process.env.JWT_SECRET);
+            res.status(200).json({ user, accessToken });
         }
- } catch (err) {
+    } catch (err) {
+        res.status(400).json("Internal Server Error");
+    }
+}
+);
+
+
+router.get('/login', async (req, res) => {
+
+  
+    try {
+
+        let user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            const comparepassword = bcrypt.compareSync(req.body.password, user.password);
+            if(!comparepassword){
+                return res.status(200).json("Wrong password");
+            }else{
+                jwt.sign({ id: user._id, username: user.username },
+                process.env.JWT_SECRET);
+            }
+            const {password, ...others} = user._doc;
+            return res.status(200).json({others});
+        } 
+    } catch (err) {
         res.status(400).json("Internal Server Error");
     }
 }
