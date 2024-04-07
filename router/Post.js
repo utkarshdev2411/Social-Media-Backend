4a8ec2003cf8053f07ae0d5b3070c91ab0f28792
Utkarsh Sharma <utkarshdev2411@gmail.com>
2024-04-07 22:55:49 +0530

    follow and unfollow

--- router/Post.js
+++ router/Post.js
@@ -39,43 +39,74 @@ router.post('/all/post/by/user', verifyToken, async (req, res) => {
 });
 
 router.put('/:id/like', verifyToken, async (req, res) => {
-    
-        try {
-            const post = await Post.findById(req.params.id);
-            if (post.likes.includes(req.user.id)) {
-                await post.updateOne({ $pull: { likes: req.user.id } });
-                res.status(200).json("Post disliked");
+
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.includes(req.user.id)) {
+            await post.updateOne({ $pull: { likes: req.user.id } });
+            res.status(200).json("Post disliked");
+        } else {
+            await post.updateOne({ $push: { likes: req.user.id } });
+            res.status(200).json("Post liked");
+        }
+    } catch (error) {
+        res.status(500).json("Internal Server Error");
+    }
+
+});
+
+router.put('/comment/post', verifyToken, async (req, res) => {
+
+    try {
+        const { comment, postid, profile } = req.body;
+
+        const comments = {
+            user: req.user.id,
+            username: req.user.username,
+            profile,
+            comment
+        }
+        const post = await Post.findById(postid);
+        if (!post) {
+            return res.status(200).json("Post not found");
+        }
+        post.comments.push(comments);
+        await post.save();
+        return res.status(200).json(post);
+
+    } catch (error) {
+        res.status(500).json("Internal Server Error");
+    }
+
+});
+
+
+
+router.put("/:id/follow", verifyToken, async (req, res) => {
+    try {
+        if (req.user.id !== req.params.id) {
+
+            const user = await User.findById(req.params.id);
+            const otheruser = await User.findById(req.body.user);
+            if (!user.followers.includes(req.user.id)) {
+                await user.updateOne({ $push: { followers: req.body.user } });
+                await otheruser.updateOne({ $push: { following: req.params.id } });
+                res.status(200).json("user has been followed");
             } else {
-                await post.updateOne({ $push: { likes: req.user.id } });
-                res.status(200).json("Post liked");
+                await user.updateOne({ $pull: { following: req.body.user } });
+                await otheruser.updateOne({ $pull: { followers: req.params.id } });
+                res.status(200).json("user has been unfollowed");
             }
-        } catch (error) {
-            res.status(500).json("Internal Server Error");
         }
-    
-    });
-    
-    router.put('/comment/post', verifyToken, async (req, res) => {
-
-        try {
-            const { comment, postid, profile } = req.body;
-
-            const comments = {
-                user: req.user.id,
-                username: req.user.username,
-                profile,
-                comment
-            }
-            const post = await Post.findById(postid);
-            post.comments.push(comments);
-            await post.save();
-            return res.status(200).json(post);
-
-        } catch (error) {
-            res.status(500).json("Internal Server Error");
-        }
-
-    });
+    } catch (err) {
+        res.status(500).json("Internal Server Error");
+    }
+});
 
 
-    module.exports = router;
+
+
+
+
+
+module.exports = router;
